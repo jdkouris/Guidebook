07b8489edc1ab42c3d7a84b10aace77c5ae2cccc
John Kouris <jdkouris@gmail.com>
2020-10-12 08:27:14 -0700

    Set up app structure and necessary files

---
+++ Guidebook/Helpers/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Guidebook
+//
+//  Created by John Kouris on 10/12/20.
+//
+
+import Foundation
+
+class Constants {
+    static var INFO_VIEWCONTROLLER = "InfoVC"
+    static var MAP_VIEWCONTROLLER = "MapVC"
+    static var NOTES_VIEWCONTROLLER = "NotesVC"
+}
