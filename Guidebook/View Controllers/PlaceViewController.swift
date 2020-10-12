c3f8dc48654621a99506027819a08c095fe87bb6
John Kouris <jdkouris@gmail.com>
2020-10-12 11:44:32 -0700

    Set up switching between child view controllers

--- Guidebook/View Controllers/PlaceViewController.swift
+++ Guidebook/View Controllers/PlaceViewController.swift
@@ -8,22 +8,82 @@
 import UIKit
 
 class PlaceViewController: UIViewController {
+    
+    // MARK: - Variables and Properties
+    
+    @IBOutlet weak var placeImageView: UIImageView!
+    @IBOutlet weak var placeNameLabel: UILabel!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var containerView: UIView!
+    
+    lazy var infoViewController: InfoViewController = {
+        return self.storyboard?.instantiateViewController(identifier: Constants.INFO_VIEWCONTROLLER) as! InfoViewController
+    }()
+    
+    lazy var mapViewController: MapViewController = {
+        return self.storyboard?.instantiateViewController(identifier: Constants.MAP_VIEWCONTROLLER) as! MapViewController
+    }()
+    
+    lazy var notesViewController: NotesViewController = {
+        return self.storyboard?.instantiateViewController(identifier: Constants.NOTES_VIEWCONTROLLER) as! NotesViewController
+    }()
+    
+    var place: Place?
+    
+    
+    // MARK: - ViewController lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        guard let place = place else { return }
+        
+        // Set the image
+        if place.imageName != nil {
+            placeImageView.image = UIImage(named: place.imageName!)
+        }
+        
+        // Set the name
+        placeNameLabel.text = place.name
+        
+        // Create an instance of the info view controller
+        switchChildViewControllers(infoViewController)
+        
     }
-    */
-
+    
+    // MARK: - Methods
+    
+    private func switchChildViewControllers(_ childVC: UIViewController) {
+        // Add it as a child view controller of this one
+        addChild(childVC)
+        
+        // Add its view as a subview of the container view
+        containerView.addSubview(childVC.view)
+        
+        // Set its frame and sizing
+        childVC.view.frame = containerView.bounds
+        childVC.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        // Indicate that it's now a child view controller
+        childVC.didMove(toParent: self)
+    }
+    
+    
+    @IBAction func segmentChanged(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            switchChildViewControllers(infoViewController)
+        case 1:
+            switchChildViewControllers(mapViewController)
+        case 2:
+            switchChildViewControllers(notesViewController)
+        default:
+            switchChildViewControllers(infoViewController)
+        }
+    }
+    
 }
